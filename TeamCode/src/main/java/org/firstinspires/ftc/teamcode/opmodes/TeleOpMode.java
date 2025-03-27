e1b401dc10e4d539eb109462d4840771955a9bd2
Lakshya <lakshyakhandelwal28@gmail.com>
2025-03-26 20:02:27 -0700

    inited chassis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOpMode.java
@@ -66,9 +66,9 @@ public class TeleOpMode extends OpMode {
         arm.setPivotPowerAuto();
         arm.setSlidePowerAuto();
         // Read inputs for controlling chassis
-        double y = AvyuktResponseCurve(gamepad1.left_stick_y);
-        double x = -AvyuktResponseCurve(gamepad1.left_stick_x);
-        double rx = -AvyuktResponseCurve(gamepad1.right_stick_x);
+        double y = -AvyuktResponseCurve(gamepad1.left_stick_y);
+        double x = AvyuktResponseCurve(gamepad1.left_stick_x);
+        double rx = AvyuktResponseCurve(gamepad1.right_stick_x);
         double y2 = IshaanResponseCurve(gamepad2.left_stick_y);
         double y3 = IshaanResponseCurve(gamepad2.right_stick_y);
 
